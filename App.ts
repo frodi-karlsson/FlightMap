9a6017ae9d05d51fac5f352fed2942ffbc2c0d58
frodikarlsson <fr8125ka-s@student.lu.se>
2022-01-08 12:55:40 +0000

    Add barebones

---
+++ App.ts
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import FlightMapContainer from './map';
+
+
+ReactDOM.render(
+    FlightMapContainer,
+    document.getElementById('flight-map')
+);
